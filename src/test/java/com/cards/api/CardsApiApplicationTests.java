b7b53bb21d86b5a103fe9019b7e6f63d3886c035
biswo <biswo.dev98@gmail.com>
2025-04-26 19:44:16 +0530

    initial commit

---
+++ src/test/java/com/cards/api/CardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cards.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
